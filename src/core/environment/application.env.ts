1e1fd77e6f0d9449fadebf6194340fb1a2f4149e
davidpozoio <pozodavid060@gmail.com>
2025-02-04 14:24:54 -0500

    feat: validate applicationConfig

--- src/core/environment/application.env.ts
+++ src/core/environment/application.env.ts
@@ -2,6 +2,7 @@ import yaml from "js-yaml";
 import fs from "node:fs";
 import type { ApplicationConfig } from "../types/environment";
 import convertKeysToCamelCase from "../utils/convert-keys-to-camel-case";
+import { z } from "zod";
 
 const applicationFile = fs.readFileSync("application.config.yml", {
   encoding: "utf-8",
@@ -12,5 +13,14 @@ const data = yaml.load(applicationFile);
 const { application: applicationConfig } = convertKeysToCamelCase(
   data,
 ) as ApplicationConfig;
+
+const applicationSchema = z.object({
+  spaMode: z.coerce.boolean().default(true),
+  test: z.object({
+    enableDefaultDatabase: z.coerce.boolean().default(false),
+  }),
+});
+
+applicationSchema.parse(applicationConfig);
 
 export default applicationConfig;
