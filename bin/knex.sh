3c4faa8aa766c17e0af2a7f61b1daa98909972d7
davidpozoio <pozodavid060@gmail.com>
2025-02-23 10:28:14 -0500

    chore: remove container name from commands and docker-compose

--- bin/knex.sh
+++ bin/knex.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-SERVICE_NAME=backend
-COMMAND="./node_modules/.bin/knex $@"
+COMMAND="npm run knex -- $@"
 
-docker exec $SERVICE_NAME sh -c "npm run esbuild -- config/knexfile.ts --outfile=knexfile.js --format=esm --packages=external --bundle --minify --platform=node"
-docker exec $SERVICE_NAME sh -c "$COMMAND"
+docker compose run  --entrypoint sh backend -c "npm run esbuild -- config/knexfile.ts --outfile=knexfile.js --format=esm --packages=external --bundle --minify --platform=node"
+
+docker compose run --entrypoint sh backend -c "$COMMAND"
