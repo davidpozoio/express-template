ca0fc0356a2792d506eb5cafa27f2a7a4988bcf6
davidpozoio <pozodavid060@gmail.com>
2025-02-25 01:09:34 -0500

    chore: remove knexfile

--- knexfile.ts
+++
@@ -1,8 +0,0 @@
-import loadKnexConfig from "@app/db/load-knex-config";
-import type { Knex } from "knex";
-
-// Update with your config settings.
-
-const config: { [key: string]: Knex.Config } = loadKnexConfig();
-
-export default config;
