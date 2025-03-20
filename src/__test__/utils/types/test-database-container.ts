356ca4d94d93394ed722173fb2007b724571ba89
davidpozoio <pozodavid060@gmail.com>
2025-03-20 10:13:36 -0500

    feat(test): add testdatabaseconfig type for getConfig

--- src/__test__/utils/types/test-database-container.ts
+++ src/__test__/utils/types/test-database-container.ts
@@ -1,8 +1,10 @@
 import type { DatabaseConfig } from "@app/core/types/database";
 
+export type TestDatabaseConfig = Omit<DatabaseConfig, "client">;
+
 export interface TestDatabaseContainer {
   stop(): Promise<void>;
   start(): Promise<void>;
   getConnectionUri(): Promise<string>;
-  getConfig(port?: number): Promise<Omit<DatabaseConfig, "client">>;
+  getConfig(port?: number): Promise<TestDatabaseConfig>;
 }
